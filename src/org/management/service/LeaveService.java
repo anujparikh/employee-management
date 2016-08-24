3d784282a1fc12c0a1c0766766ec1eb46f1040d7
anujparikh <anujparikh1@gmail.com>
2016-08-24 16:00:16 -0400

    add leave service interface

---
+++ src/org/management/service/LeaveService.java
@@ -0,0 +1,14 @@
+package org.management.service;
+
+import org.management.domain.Leave;
+
+import java.util.List;
+
+public interface LeaveService extends GenericDao{
+
+    List<Leave> retrieveLeavesOnEmployeeId(String employeeId);
+
+    List<Leave> retrieveLeavesPendingForApproval(String employeeId);
+
+    List<Leave> retrieveLeavesOnStatusEmployeeId(String employeeId, String status);
+}
